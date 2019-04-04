d56d71bc81ab90266bba8a8a4adbf2aba7b3b82f
chlngers <48171884+chlngers@users.noreply.github.com>
2019-04-04 20:30:06 +0900

    Add files via upload

---
+++ audioplayer.js
@@ -0,0 +1,165 @@
+function playFast(num) {
+    var audio = document.getElementById('fastAudio'+num);
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.currentTime = 0
+    }
+}
+
+function playSlow(num) {
+    var audio = document.getElementById('slowAudio'+num);
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.currentTime = 0
+    }
+}
+
+function createTable(num){
+        switch(num){
+            case 1:
+                var chinesePart = chinesePart1
+                var pinyinPart = pinyinPart1
+                var koreanPart = koreanPart1
+            break;
+            case 2:
+                var chinesePart = chinesePart2
+                var pinyinPart = pinyinPart2
+                var koreanPart = koreanPart2
+            break;
+            case 3:
+            var chinesePart = chinesePart3
+            var pinyinPart = pinyinPart3
+            var koreanPart = koreanPart3
+            break;
+            case 4:
+            var chinesePart = chinesePart4
+            var pinyinPart = pinyinPart4
+            var koreanPart = koreanPart4
+        break;
+        case 5:
+            var chinesePart = chinesePart5
+            var pinyinPart = pinyinPart5
+            var koreanPart = koreanPart5
+        break;
+        case 6:
+        var chinesePart = chinesePart6
+        var pinyinPart = pinyinPart6
+        var koreanPart = koreanPart6
+    break;
+    case 7:
+        var chinesePart = chinesePart7
+        var pinyinPart = pinyinPart7
+        var koreanPart = koreanPart7
+    break;
+    case 8:
+    var chinesePart = chinesePart8
+    var pinyinPart = pinyinPart8
+    var koreanPart = koreanPart8
+    break;
+    case 9:
+    var chinesePart = chinesePart9
+    var pinyinPart = pinyinPart9
+    var koreanPart = koreanPart9
+break;
+case 10:
+    var chinesePart = chinesePart10
+    var pinyinPart = pinyinPart10
+    var koreanPart = koreanPart10
+break;
+        }
+    
+
+var body = document.body,
+    tbl  = document.createElement('table');
+tbl.style.border = '1px solid black';
+
+var counter = 0;
+var chineseCounter =0;
+var pinyinCounter =0;
+var koreanCounter =0;
+var audioCounter = 0;
+
+for(var i = 0; i < chinesePart.length*3; i++){
+    var tr = tbl.insertRow();
+    for(var j = 0; j < 2; j++){
+        var td = tr.insertCell();
+
+        if (i == counter && j!=1){
+            var br = document.createElement('br');
+            //td.setAttribute('rowspan','3');
+            var button1 = document.createElement('button');
+            button1.appendChild(document.createTextNode("빠르게"));
+            button1.setAttribute('onclick','playFast('+audioCounter+')')
+
+            var audio1 = document.createElement('audio')
+            audio1.id = 'fastAudio' + audioCounter
+            audio1.setAttribute('src', 'Fast'+num+'/'+audioCounter + '.mp3');
+
+
+            var button2 = document.createElement('button');
+            button2.appendChild(document.createTextNode("느리게")); 
+            button2.setAttribute('onclick','playSlow('+audioCounter+')')
+            button2.setAttribute('id','slowButton')
+
+            var audio2 = document.createElement('audio')
+            audio2.id = 'slowAudio' + audioCounter
+            audio2.setAttribute('src', 'Slow'+num+'/'+audioCounter + '.mp3');
+        
+
+
+            td.appendChild(button1);
+            td.appendChild(audio1);
+            td.appendChild(br);
+            td.appendChild(button2);
+            td.appendChild(audio2);
+            counter = counter+3;
+            audioCounter++;
+        }
+
+        if (j==1){
+            switch((i+3)%3){
+                case 0:
+                    td.setAttribute("class","chinese")
+                    td.appendChild(document.createTextNode(chinesePart[chineseCounter]));
+                    chineseCounter++;
+                break;
+                case 1:
+                    td.setAttribute("class","pinyin")
+                    td.appendChild(document.createTextNode(pinyinPart[pinyinCounter]));
+                    pinyinCounter++;
+                break;
+                case 2:
+                    td.setAttribute("class","korean")
+                    td.appendChild(document.createTextNode(koreanPart[koreanCounter]));
+                    koreanCounter++;
+                break;
+            }
+        }
+        td.style.border = '1px solid black'
+    }
+}
+body.appendChild(tbl);
+}
+
+document.addEventListener('play', function(e){
+var audios = document.getElementsByTagName('audio');
+for(var i = 0, len = audios.length; i < len;i++){
+    if(audios[i] != e.target){
+        audios[i].pause();
+        audios[i].currentTime = 0;
+    }
+}
+}, true);
+
+function hide(text) {
+        var elements = document.getElementsByClassName(text);
+        for (var i = 0; i < elements.length; i++){
+            if ((elements[i].style.display === "block")||(elements[i].style.display === "")){
+                elements[i].style.display = 'none';
+            } else {
+                elements[i].style.display = 'block';
+            }
+    }
+    }
---
+++ script.js
@@ -0,0 +1,190 @@
+chinesePart1 = ["你在业余时间最喜欢做什么？ ", 
+"我业余时间最喜欢在家看电视。", 
+"因为平常工作很忙，也经常加班。", 
+"每次回家以后，就没有力气去做别的事情了，所以我一般会看电视。", 
+"我会做一些好吃的菜，或者买一些零食，然后坐在沙发上和家人一边吃饭一边看电视。", 
+"我比较喜欢看新闻和电视剧。", 
+"最近在看一部很有意思的悬疑剧，叫《推理的女王》。"
+]; 
+
+pinyinPart1 = 
+["Nǐ zài yèyú shíjiān zuì xǐhuān zuò shénme?", 
+"Wǒ yèyú shíjiān zuì xǐhuān zàijiā kàn diànshì. ", 
+"Yīn wèi píngcháng gōngzuò hěn máng, yě jīngcháng jiābān. ", 
+"Měi cì huí jiā yǐhòu jiù méiyǒu lìqi qù zuò bié de shìqingle suǒyǐ wǒ yìbān huì kàn diànshì. ", 
+"Wǒ huì zuò yìxiē hǎo chī de cài, huòzhě mǎi yìxiē língshí, ránhòu zuò zài shāfā shàng hé jiārén yìbiān chīfàn yìbiān kàn diànshì. ", 
+"Wǒ bǐjiào xǐhuān kàn xīnwén hé diànshìjù. ", 
+"Zuìjìn zài kàn yí bù hěn yǒuyìsi de xuányí jù jiào “tuīlǐ de nǚwáng”."
+];
+
+koreanPart1 = ["여가 시간에 뭘 하는 것을 좋아해요?", 
+"저는 여가 시간에 집에서 TV를 보는 것을 가장 좋아해요. ", 
+"평상시에는 일이 바빠서 자주 야근을 해서 ", 
+"매번 집에 돌아가면 다른 일을 할 힘이 없어져서 저는 일반적으로 TV를 봐요. ", 
+"저는 맛있는 음식을 만들거나 간식을 좀 사서 소파에 앉아 가족과 밥을 먹으면서 TV를 봐요. ", 
+"저는 뉴스와 드라마를 보는 것을 비교적 좋아해요. ", 
+"요즘 <추리의 여왕>이라는 재미있는 미스터리 드라마를 보고 있어요."
+];
+
+chinesePart2=["昨天你做什么了？ ", 
+"昨天我在公司上班了，进行了一个重要的工作总结报告会议。", 
+"会议前两个小时我精心准备了PPT演讲资料，","总结上个月的工作进展，确定下个月的工作目标。", 
+"会议上，各个部门针对重要问题积极讨论，","最终一致通过了最佳方案，会议顺利结束。", 
+"定期召开会议，对公司发展的方向和工作效率有很大的帮助，","所以我觉得昨天是很重要的一天。"]
+pinyinPart2=["Zuó tiān nǐ zuò shén me le?",
+"Zuótiān wǒ zài gōngsī shàngbānle, jìnxíngle yígè zhòngyào de gōngzuò zǒngjié bàogào huìyì. ",
+"Huìyì qián liǎng gè xiǎoshí wǒ jīngxīn zhǔnbèile PPT yǎnjiǎng zīliào,"," zǒngjié shàng gè yuè de gōngzuò jìnzhǎn, quèdìng xià gè yuè de gōngzuò mùbiāo. ",
+"Huìyì shàng, gège bùmén zhēnduì zhòngyào wèntí jījí tǎolùn, ","zuìzhōng yízhì tōngguòle zuì jiā fāng'àn, huìyì shùnlì jiéshù. ",
+"Dìngqī zhàokāi huìyì, duì gōngsī fāzhǎn de fāngxiàng hé gōngzuò xiàolǜ yǒu hěn dà de bāngzhù, ","suǒyǐ wǒ juéde zuótiān shì hěn zhòngyào de yìtiān."]
+koreanPart2=["어제 뭐했어요?",
+"어제 저는 회사에 출근해서 중요한 업무 총결과 관련 보고 회의를 했어요. ",
+"회의 시작 두 시간 전에 저는 PPT 강연 자료를 심혈을 기울여 준비하여"," 지난달의 업무 진행 상황을 결산하고 다음 달의 업무 목표를 확정하였어요. ",
+"회의에서 각 부서는 중요한 문제에 대해 적극적으로 토론을 벌여"," 결국 최선의 방안을 만장일치로 채택하고 순조롭게 회의를 끝냈어요.",
+"정기적으로 회의를 소집하면 회사의 발전 방향과 업무 효율에 큰 도움이 되기 때문에","저는 어제가 매우 중요한 날이었다고 생각해요.",]
+
+chinesePart3=["你怎么开始学习汉语的？",
+"我今年15岁，上中学二年级。","我的学校里有中文课，所以我在学校里学的汉语。","我们学校的中文科目是必修课，期中考试和期末考试都要考，所以我必须努力学习中文。",
+"除了学校，我还自己报了辅导班，到学院去学习汉语。",
+"辅导班里的中国老师非常有意思，而且给我介绍了很多中国文化。",
+"我有时间还自己在网上看中国的电视剧、电影、听中国音乐。", "我希望自己的汉语说得越来越好，今后去中国旅游。"]
+pinyinPart3=["Nǐ zěnme kāishi xuéxí hànyǔ de?",
+"Wǒ jīnnián 15 suì, shàng zhōngxué èr niánjí. ",
+"Wǒ de xuéxiào li yǒu zhòng wén kè, suǒyǐ wǒ zài xuéxiào lǐ xué de hànyǔ.",
+"Wǒmen xuéxiào de zhōngwén kēmù shì bìxiū kè, qīzhōng kǎoshì hé qīmò kǎoshì dōu yào kǎo, suǒyǐ wǒ bìxū nǔlì xuéxí zhōngwén.", "chúle xuéxiào, wǒ hái zìjǐ bàole fǔdǎo bān, dào xuéyuàn qù xuéxí hànyǔ. ",
+"Fǔdǎo bān lǐ de zhōngguó lǎoshī fēicháng yǒuyìsi, érqiě gěi wǒ jièshàole hěnduō zhōngguó wénhuà. ",
+"Wǒ yǒu shíjiān hái zìjǐ zài wǎngshàng kàn zhòng guó de diànshìjù, diànyǐng, tīng zhōngguó yīnyuè. ",
+"Wǒ xīwàng zìjǐ de hànyǔ shuō dé yuè lái yuè hǎo, jīnhòu qù zhōngguó lǚyóu."]
+koreanPart3=["어떻게 중국어를 배우게 됐어요?",
+"저는 올해 15살이고 중학교 2학년을 다니고 있어요. ",
+"저희 학교에는 중국어 수업이 있어서 저는 학교에서 중국어를 배우고 있어요. ",
+"우리 학교의 중국어 과목은 필수과목으로 중간고사와 기말고사 모두 시험을 치뤄야 하기 때문에 저는 반드시 열심히 중국어를 공부해야 해요. ",
+"학교 말고도 저는 스스로 학원에 가서 중국어 공부를 했어요. ",
+"학원에 있는 중국인 선생님은 아주 재미있고 또 나에게 많은 중국 문화를 소개시켜 주셨어요. ",
+"저는 시간이 나면 인터넷으로 중국의 드라마와 영화를 보고 중국 음악을 듣기도 했어요. ",
+"제가 중국어 실력이 점점 더 좋아지면 내년에 중국을 여행하고 싶어요."]
+
+chinesePart4=["你什么时候最忙？ ",
+"我是中学生，来自韩国。",
+"所以我期中考试和期末考试的时候最忙。",
+"在韩国，期中考试和期末考试的成绩直接影响了你能不能去好的高中和大学。",
+"所以，考试期间我会特别忙。",
+"我考试前两周就不玩儿了，开始准备考试，","这期间我经常晚上12点睡觉，有的时候没有时间吃饭。",
+"因为在韩国，学习压力很大，所以，我考试期间不敢放松。","如果遇到不会的地方，我会去问老师，或者到网上找答案。",
+"考完试以后，我们会放假。","这时我就可以好好休息一下了。",
+"放假期间我会和家人去国外旅游。"]
+pinyinPart4=["Nǐ shénme shíhòu zuì máng?",
+"Wǒ shì zhōngxuéshēng, láizì hánguó. ",
+"Suǒyǐ wǒ qīzhōng kǎoshì hé qīmò kǎoshì de shíhòu zuì máng. ",
+"Zài hánguó, qīzhōng kǎoshì hé qīmò kǎoshì de chéngjī zhíjiē yǐngxiǎngle nǐ néng bùnéng qù hǎo de gāozhōng hé dàxué.",
+"Suǒyǐ, kǎoshì qījiān wǒ huì tèbié máng. ",
+"Wǒ kǎoshì qián liǎng zhōu jiù bù wán erle, kāishǐ zhǔnbèi kǎoshì, ","zhè qījiān wǒ jīngcháng wǎnshàng 12 diǎn shuìjiào, yǒu de shíhòu méiyǒu shíjiān chīfàn. ",
+"Yīnwèi zài hánguó, xuéxí yālì hěn dà, suǒyǐ, wǒ kǎoshì qījiān bù gǎn fàngsōng. ",
+"Rúguǒ yù dào bú huì de dìfāng, wǒ huì qù wèn lǎoshī, huòzhě dào wǎngshàng zhǎo dá'àn. ",
+"Kǎo wán shì yǐhòu, wǒmen huì fàngjià. ",
+"Zhè shí wǒ jiù kěyǐ hǎohǎo xiūxi yíxiàle. ",
+"Fàngjià qījiān wǒ huì hé jiārén qù guówài lǚyóu."]
+koreanPart4=["언제 제일 바빠요?",
+"저는 한국에서 온 중학생이에요. ",
+"그래서 저는 중간고사와 기말고사 때가 제일 바빠요. ",
+"한국에서의 중간고사와 기말고사 성적은 제가 좋은 고등학교와 대학교에 갈 수 있는지에 직접적인 영향을 줘요. ",
+"그래서 시험 기간에 저는 특별히 바빠요."," 저는 시험을 보기 전 2주 동안 놀지 않고 시험을 준비하기 시작했어요.", "이 기간 동안 저는 늘 밤 12시에 잠을 자고 어떤 때는 밥을 먹을 시간도 없어요. ",
+"한국에서는 공부 스트레스가 심해 시험 기간에 긴장을 늦출 수 없어요. ",
+"만약 모르는 것이 있으면 저는 선생님에게 묻고 인터넷으로 답을 찾아요. ",
+"시험이 끝난 후에 우리는 방학을 해요. ",
+"이때 저는 잘 쉴 수 있어요. ",
+"휴가 기간에 저는 가족과 함께 해외여행을 갈 거예요."]
+
+chinesePart5=["你起床后一般做什么？",
+"我起床后，就冲一杯咖啡，然后去洗漱化妆，之后为出门挑选衣服。",
+"在去工作的地铁上我还会用手机看一会儿新闻。",
+"每天早上一杯咖啡可以让我整天都充满精力，","也可以让我头脑尽快清醒，投入一天的工作中。",
+"我的工作很忙，每天必须精力充沛地投入工作，","化美美的妆也可以让我更加自信地和别人交流。"]
+pinyinPart5=["Nǐ qǐchuáng hòu yìbān zuò shénme?",
+"Wǒ qǐchuáng hòu, jiù chōng yìbēi kāfēi, ránhòu qù xǐshù huàzhuāng, zhīhòu wèi chūmén tiāoxuǎn yīfu. ",
+"Zài qù gōngzuò de dìtiě shàng wǒ hái huì yòng shǒujī kàn yíhuìr xīnwén. ",
+"Měitiān zǎoshang yìbēi kāfēi kěyǐ ràng wǒ zhěnggè tiān dōu chōngmǎn jīnglì,", "yě kěyǐ ràng wǒ tóunǎo jǐnkuài qīngxǐng, tóurù yìtiān de gōngzuò zhōng. ",
+"wǒ de gōngzuò hěn máng, měitiān bìxū jīnglì chōngpèi de tóurù gōngzuò,"," huà měiměi de zhuāng yě kěyǐ ràng wǒ gèngjiā zìxìn de hé biérén jiāoliú."]
+koreanPart5=["일어난 후에 제일 먼저 무엇을 하나요?",
+"저는 일어난 후에 커피 한 잔을 마시고 나서 세수하고 화장한 후에 집 밖으로 나가기 위해 옷을 골라요. ",
+"일하러 가는 지하철에서 저는 또 휴대전화로 잠시 뉴스를 봐요. ",
+"매일 아침 커피 한 잔으로 하루 종일 에너지를 충전할 수 있고 ","정신을 빨리 차려 하루 일과에 몰두할 수도 있어요. ",
+"제가 일은 매우 바빠서 매일 정력을 쏟아야 하고"," 아름다운 화장을 하는 것은 나로 하여금 더욱 자신있게 다른 사람과 교류하게 해줘요."]
+
+chinesePart6=["你喜欢看新闻吗？为什么？",
+"我喜欢看新闻，首先我认为看新闻可以让我了解到最新的消息，","这样在和别人交流的时候有话题可以聊天。",
+"再者，看新闻也可以让我跟上社会的脚步，","不仅仅是自己国家的新鲜事物，更可以了解到世界各地有趣的事情，","让我不用环球旅游就可以用最方便的方式了解世界。",
+"最后我认为看新闻是一件有意思的事情，","给自己单调重复的上班生活提供新鲜的信息，","让我的一整天都感到很充实。"]
+pinyinPart6=["Nǐ xǐhuān kàn xīnwén ma? Wèishénme?",
+"Wǒ xǐhuān kàn xīnwén, shǒuxiān wǒ rènwéi kàn xīnwén kěyǐ ràng wǒ liǎojiě dào zuìxīn de xiāoxi,", "zhèyàng zài hé biérén jiāoliú de shíhòu yǒu huàtí kěyǐ liáotiān. ",
+"Zài zhě, kàn xīnwén yě kěyǐ ràng wǒ gēn shàng shèhuì de jiǎobù,", "bùjǐn jǐn shì zìjǐ guójiā de xīnxiān shìwù, gèng kěyǐ liǎojiě dào shìjiè gèdì yǒuqù de shìqing," ,"ràng wǒ búyòng huánqiú lǚyóu jiù kěyǐ yòng zuì fāngbiàn de fāngshì liǎojiě shìjiè.",
+"Zuìhòu wǒ rènwéi kàn xīnwén shì yí jiàn yǒuyìsi de shìqing,","gěi zìjǐ dāndiào chóngfù de shàngbān shēnghuó tígōng xīnxiān de xìnxī, ","ràng wǒ de yì zhěng tiān dū gǎndào hěn chōngshí."]
+koreanPart6=["뉴스를 즐겨 보나요? 왜 그런가요?",
+"저는 뉴스를 좋아해요. 우선 뉴스를 보면 최신 소식을 알 수 있고 ","그러면 다른 사람과 얘기할 때 할 말이 있어요. ",
+"또 뉴스를 보는 것을 통해 사회를 알 수 있고", "우리 나라의 새로운 일 뿐만 아니라 세계 각지의 재미있는 일들을 함께 이해할 수 있어요.", "세계여행을 하지 않고도 세계를 가장 편리한 방식으로 이해할 수 있어요. ",
+"마지막으로 뉴스를 보는 게 재밌다고 생각하고,", "단조롭고 반복되는 출근 생활에 신선한 정보를 제공해 ","하루 종일 보람을 느껴요."]
+
+chinesePart7=["你一般在网上买什么东西？",
+"我已经工作两年了，我喜欢在网上买东西。",
+"因为平时工作很忙，下班也很晚，回家只想早点休息，","放假的日子经常和朋友聊聊天喝喝咖啡，","所以很少有机会可以自己去买东西。",
+"在网上买东西非常方便，不需要花费太多的时间和精力","我会常常购买一些日用品，包括饮用水，","如果自己去买会很重，很难带回家，网上买可以直接送到家门口，","而且，网上的商品种类非常多，","很多商品券可以用，商家也常常做活动，所以我也会购买一些零食。"]
+pinyinPart7=["Nǐ yìbān zài wǎngshàng mǎi shénme dōngxi?",
+"Wǒ yǐjīng gōngzuò liǎng niánle, wǒ xǐhuān zài wǎngshàng mǎi dōngxi. ",
+"Yīn wèi píngshí gōngzuò hěn máng, xiàbān yě hěn wǎn, huí jiā zhǐ xiǎng zǎodiǎn xiūxi,", "fàngjià de rìzi jīngcháng hé péngyou liáo liáotiān hē hē kāfēi," ,"suǒyǐ hěn shǎo yǒu jīhuì kěyǐ zìjǐ qù mǎi dōngxi. ",
+"Zài wǎngshàng mǎi dōngxi fēicháng fāngbiàn, bù xūyào huāfèi tài duō de shíjiān hé jīnglì,","wǒ huì chángcháng gòumǎi yìxiē rìyòngpǐn, bāokuò yǐnyòng shuǐ,", "rúguǒ zìjǐ qù mǎi huì hěn zhòng, hěn nán dài huí jiā, wǎngshàng mǎi kěyǐ zhíjiē sòng dàojiā ménkǒu,","érqiě, wǎngshàng de shāngpǐn zhǒnglèi fēicháng duō,"," hěnduō shāngpǐn quàn kěyǐ yòng, shāngjiā yě chángcháng zuò huódòng, suǒyǐ wǒ yě huì gòumǎi yìxiē língshí."]
+koreanPart7=["보통 인터넷에서 뭘 사요?",
+"저는 이제 일한지 2년이 됐는데 컴퓨터로 인터넷에서 물건을 사는 것을 좋아해요. ",
+"평소 일이 바빠서 퇴근도 늦었는데 집에 가서 일찍 쉬고 싶다는 생각만 하고"," 쉬는 날엔 친구들과 자주 이야기를 나누고 커피를 마셔서","혼자 쇼핑을 할 수 있는 기회가 드물어요. ",
+"인터넷으로 물건을 사는 것이 아주 편하고 시간이 많이 필요하지 않아, ","저는 자주 생활용품을 구입하는데 식수도 인터넷으로 사요. ",
+"혼자 가서 사오면 무거워서 집에 가기 어렵지만 인터넷으로 사면 바로 집 앞까지 배달해줘요. ",
+"뿐만 아니라 인터넷 상의 상품 종류도 매우 많아요.","사용할 수 있는 쿠폰도 많고 행사도 많아서 저는 간식도 가끔 사요."]
+
+chinesePart8=["你有什么不好的习惯？",
+"我的坏习惯比较多，","比如不爱运动、爱喝咖啡、爱暴饮暴食、经常晚睡、天天看手机和电脑等等。",
+"我有一个不好的习惯就是每天喝好几杯咖啡，","上大学的时候我没有喝咖啡的习惯，","但是上班以后，我工作压力比较大，","所以我基本上一天要喝四五杯咖啡。",
+"现在不喝咖啡就觉得没有精神，","而且有时候是空腹喝咖啡，对身体很不好。",
+"我打算以后戒掉咖啡，以后多喝茶和果汁。"]
+pinyinPart8=["Nǐ yǒu shén me bù hǎo de xíguàn?",
+"Wǒ de huài xíguàn bǐjiào duō,","bǐrú bù ài yùndòng, ài hē kāfēi, ài bào yǐn bàoshí, jīngcháng wǎn shuì, tiāntiān kàn shǒujī hé diànnǎo děng děng. ",
+"Wǒ yǒu yígè bù hǎo de xíguàn jiùshì měitiān hē hǎojǐ bēi kāfēi,", "shàng dàxué de shíhòu wǒ méiyǒu hē kāfēi de xíguàn,"," dànshì shàngbān yǐhòu, wǒ gōngzuò yālì bǐjiào dà,","suǒyǐ wǒ jīběn shàng yìtiān yào hē sìwǔ bēi kāfēi. ",
+"Xiànzài bù hē kāfēi jiù juéde méiyǒu jīngshén,", "érqiě yǒu shíhòu shì kōngfù hē kāfēi, duì shēntǐ hěn bù hǎo. ",
+"Wǒ dǎsuàn yǐhòu jiè diào kāfēi, yǐhòu duō hē chá hé guǒzhī."]
+koreanPart8=["어떤 나쁜 습관이 있어요?",
+"저는 나쁜 습관이 아주 많아요.", "예를 들면 운동을 좋아하지 않고 커피 마시는 것, 폭식하는 것, 자주 늦게 자는 것, 매일 휴대전화와 컴퓨터를 보는 것 등이 있어요. ",
+"저는 매일 커피를 몇 잔 마시는 나쁜 습관이 있는데 ","대학교에 다닐 때는 커피를 마시는 습관이 없었는데" ,"출근하게 된 후에는 업무 스트레스가 비교적 크기 때문에"," 저는 거의 하루에 커피를 네댓 잔 마셔야 해요. ",
+"지금은 커피를 마시지 않으면 정신이 없지만" ,"빈 속에 커피를 마시면 몸에 좋지 않아요. ",
+"저는 나중에 커피를 끊고 차와 주스를 많이 마실 계획이에요."]
+
+chinesePart9=["你最想做什么工作？为什么？",
+"我是中学生，来自韩国，我想做游戏软件开发的工作，","因为我非常喜欢玩儿游戏，如果工作就是玩儿游戏，那我一定可以做得很好。",
+"并且，每次玩游戏的时候我总能找到很多需要改进的地方,","我还会给游戏公司写邮件告知他们我的游戏体验感，","目前已经有两款游戏根据我的意见改进了游戏内容，","这让我更想去做游戏软件开发的工作了。",
+"每天在学校里，我就很不喜欢学习历史这样的学科，但是我对数学就很感兴趣，","这也帮助我在以后可以做软件开发的工作。",
+"我希望可以和自己有相同爱好的人一起努力。"]
+pinyinPart9=["Nǐ zuì xiǎng zuò shénme gōngzuò? Wèishénme?",
+"Wǒ shì zhōngxuéshēng, láizì hánguó, wǒ xiǎng zuò yóuxì ruǎnjiàn kāifā de gōngzuò,", "yīnwèi wǒ fēicháng xǐhuān wánr yóuxì, rúguǒ gōngzuò jiùshì wánr yóuxì, nà wǒ yídìng kěyǐ zuò de hěn hǎo. ",
+"Bìngqiě, měi cì wánr yóuxì de shíhòu wǒ zǒng néng zhǎodào hěnduō xūyào gǎijìn de dìfang,"," wǒ hái huì gěi yóuxì gōngsī xiě yóujiàn gàozhī tāmen wǒ de yóuxì tǐyàn gǎn," ,"mùqián yǐjīng yǒu liǎng kuǎn yóuxì gēnjù wǒ de yìjiàn gǎijìnle yóuxì nèiróng,"," zhè ràng wǒ gèng xiǎng qù zuò yóuxì ruǎnjiàn kāifā de gōngzuòle. ",
+"Měitiān zài xuéxiào lǐ, wǒ jiù hěn bù xǐhuān xuéxí lìshǐ zhèyàng de xuékē, dànshì wǒ duì shùxué jiù hěn gǎn xìngqù,", "zhè yě bāngzhù wǒ zài yǐhòu kěyǐ zuò ruǎnjiàn kāifā de gōngzuò. ",
+"Wǒ xīwàng kěyǐ hé zìjǐ yǒu xiāngtóng àihào de rén yìqǐ nǔlì."]
+koreanPart9=["어떤 일을 가장 하고 싶어요? 왜요?",
+"저는 한국에서 온 중학생이에요. 저는 게임 소프트웨어 개발과 관련 일을 하고 싶어요. ",
+"왜냐하면 저는 게임을 매우 좋아하기 때문이에요. 일이 게임이라면 저는 꼭 잘 할 수 있다고 생각해요. ",
+"또한, 게임을 할 때마다 저는 많은 개선할 점을 찾을 수 있어요. ",
+"저는 게임 회사에 저의 게임 체험 소감을 이메일로 보내요. ","현재 이미 두 게임이 제 의견에 의해 게임 내용을 개선했어요. ",
+"그래서 게임 소프트웨어 개발 일을 더 하고 싶어졌어요.","학교에서 저는 역사 같은 과목을 좋아하지 않고 수학에 대해 매우 관심이 있어요. ",
+"이는 앞으로 소프트웨어 개발과 관련 일을 할 수 있도록 도와줘요. ",
+"저는 같은 취미가 있는 사람과 같이 노력할 수 있길 바래요."]
+
+chinesePart10=["你不高兴的时候，会做什么？",
+"我不高兴的时候会找朋友聊天，","因为我认为如果一直一个人，无法吐露自己的心情，","就不能缓解不高兴的情绪，会更不高兴，","所以和朋友聊天，可以让我很快忘记不高兴的事情。",
+"但是不能总在不高兴的时候找朋友聊天，别人也有别人的苦恼，","所以我常常在不高兴的时候去运动，","因为我认为运动可以缓解压力，释放自己，","运动还能使我感到快乐，也能让我保持健康，","这样就算有不高兴的事情，我都会去面对。",
+"但很多时候我都没有时间不高兴，因为我很忙。"]
+pinyinPart10=["Nǐ bù gāoxìng de shíhòu, huì zuò shénme?",
+"Wǒ bù gāoxìng de shíhòu huì zhǎo péngyou liáotiān, ","yīnwèi wǒ rènwéi rúguǒ yìzhí yígè rén, wúfǎ tǔlù zìjǐ de xīnqíng,","jiù bùnéng huǎnjiě bù gāoxìng de qíngxù, huì gèng bù gāoxìng, ","suǒyǐ hé péngyou liáotiān, kěyǐ ràng wǒ hěn kuài wàngjì bù gāoxìng de shìqing. ",
+"Dànshì bùnéng zǒng zài bù gāoxìng de shíhòu zhǎo péngyou liáotiān, biérén yěyǒu biérén de kǔnǎo, ","suǒyǐ wǒ chángcháng zài bù gāoxìng de shíhòu qù yùndòng, ","yīnwèi wǒ rènwéi yùndòng kěyǐ huǎnjiě yālì, shìfàng zìjǐ, ","yùndòng hái néng shǐ wǒ gǎndào kuàilè, yě néng ràng wǒ bǎochí jiànkāng,","zhèyàng jiùsuàn yǒu bù gāoxìng de shìqing, wǒ dōuhuì qù miàn duì. ",
+"Dàn hěnduō shíhòu wǒ dōu méiyǒu shíjiān bù gāoxìng, yīnwèi wǒ  hěn máng."]
+koreanPart10=["기분이 안 좋을 때 무얼 하나요?",
+"내가 기분 나쁠 때 친구를 찾아가 이야기를 해요.",
+"왜냐하면 기분이 나쁠 때 자꾸 내 기분을 털어놓지 못하면","기쁘지 않은 마음을 풀지 못해서 기분이 나빠질 것 같기 때문이에요. ",
+"친구들과 이야기를 나누다 보면 기분 나쁜 일을 금방 잊을 수 있어요. ",
+"하지만 기분 나쁠 때마다 항상 친구를 찾아 이야기할 수 없고 다른 사람도 고민이 있어서","기분이 좋지 않을 때는 운동을 많이 해요. ",
+"왜냐하면 운동을 하면 스트레스를 풀고 나의 기분도 풀어 주고,","운동을 하면 즐거워지며 건강을 유지할 수 있어요. ",
+"이렇게 하면 나는 불쾌한 일이 있어도 직면할 수 있어요." ,"그러나 대부분의 경우 바빠서 기분이 안 좋을 시간이 없어요."]
